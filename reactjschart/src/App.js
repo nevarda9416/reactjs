082ff26fcbf2cc3b7276e38c1f8b7311fcdfef2b
tudao@5nhatnhat.com <nevarda9416>
2021-12-07 17:13:16 +0700

    - [ReactJS + Laravel Chart Example Kết Hợp]: Using chart.js to create bar chart, line chart, pie & doughnut chart

--- reactjschart/src/App.js
+++ reactjschart/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import BarChartComponent from './components/BarChartComponent';
+import PieChartComponent from './components/PieChartComponent';
+import LineChartComponent from './components/LineChartComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>LineChart</h1>
+                <LineChartComponent/>
+                <h1>BarChart</h1>
+                <BarChartComponent/>
+                <h1>PieChart</h1>
+                <PieChartComponent/>
+            </div>
+        );
+    }
 }
 
 export default App;
