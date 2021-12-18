281ea22c4c7a6ac74e11d64b38b34ce6da41921a
tudao@5nhatnhat.com <nevarda9416>
2021-12-18 11:41:24 +0700

    - [ReactJS + Laravel Chart Example Kết Hợp]: Get lại data từ api bên project vuejs + laravel kết hợp

--- reactjschart/src/components/BarChartComponent.js
+++ reactjschart/src/components/BarChartComponent.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Bar} from 'react-chartjs-2';
 import axios from 'axios';
 import {ArcElement} from 'chart.js';
 import Chart from 'chart.js/auto';
+
 Chart.register(ArcElement);
 
-export default class BarChartComponent extends Component
-{
+export default class BarChartComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -15,34 +15,34 @@ export default class BarChartComponent extends Component
     }
 
     componentDidMount() {
-        axios.get(`http://127.0.0.1:8000/coins`)
+        axios.get(`http://127.0.0.1:8000/golds`)
             .then(res => {
-                const data = res.data.golds[0].value;console.log(data);
+                const data = res.data;
                 let brands = [];
                 let sells = [];
                 if (data) {
                     data.forEach(element => {
                         brands.push(element.brand + ' (' + element.company + ')');
-                        sells.push(Number(element.sell.replace(/[^0-9.-]+/g, "")));
+                        sells.push(Number(element.price));
                     });
                 }
                 this.setState({
                     chartData: {
                         labels: brands,
-                        datasets:[
+                        datasets: [
                             {
-                                label:'Giá vàng trong nước bán ra ngày ' + res.data.golds[0].updated,
-                                data: sells ,
-                                backgroundColor: 'yellow'
+                                label: 'Giá vàng trong nước bán ra (VNĐ/lượng)',
+                                backgroundColor: 'yellow',
+                                data: sells
                             }
                         ]
                     }
                 });
             })
     }
-    render()
-    {
-        return(
+
+    render() {
+        return (
             <div>
                 {
                     Object.keys(this.state.chartData).length &&
