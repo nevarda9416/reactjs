872aaf8bc39e6257c52fb8f5b45537ce59cd08bd
tudao@5nhatnhat.com <nevarda9416>
2021-12-20 17:19:07 +0700

    - [ReactJS + Redux Toolkit Example Kết Hợp]: ------------------- Clear kiến trúc redux có config store, module actions, reducer và view ++++++++++++++++++++ Khởi tạo kiến trúc redux MVC mới

--- reactjsredux/src/counter/view.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const View = ({count, handleDecrementClick, handleIncrementClick}) => (
-    <div>
-        <h1>Hello world React & Redux!<br/> {count}</h1>
-        <button onClick={handleDecrementClick}>Decrement</button>
-        <button onClick={handleIncrementClick}>Increment</button>
-    </div>
-);
