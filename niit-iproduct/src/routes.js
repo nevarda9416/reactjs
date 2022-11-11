9cdb1aa428470a8a9c245c5daa5031461bc24acf
DAO TIEN TU <daotientu@gmail.com>
2022-11-11 21:00:44 +0700

    - [ReactJS]: Category Component

--- niit-iproduct/src/routes.js
+++ niit-iproduct/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+const Category = React.lazy(() => import('./views/category/Category'))
 const GoogleTrend = React.lazy(() => import('./views/dashboard/GoogleTrend'))
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
 const Typography = React.lazy(() => import('./views/theme/typography/Typography'))
@@ -54,6 +55,7 @@ const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/category', name: 'Danh mục sản phẩm', element: Category },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/google/trend', name: 'Google Trend', element: GoogleTrend },
   { path: '/theme/colors', name: 'Colors', element: Colors },
