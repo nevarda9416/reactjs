e17d3c96f48339ad6ccd9b81b1caed05f7cb27bf
TuDT <tudt@ecomobi.com>
2022-12-15 13:28:08 +0700

    - [ExpressJS]: All seeding data (3)

--- niit-iproduct/nodejs_expressjs/database/mongodb/seed/data.js
+++ niit-iproduct/nodejs_expressjs/database/mongodb/seed/data.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.get('/data/create', function (req, res) {
+  // List sample categories
   const listCategories = [
     {name: 'Smartphones', dbname: 'smartphones', description: 'Smartphones'},
     {name: 'Máy tính bảng', dbname: 'maytinhbang', description: 'Máy tính bảng'},
@@ -27,16 +28,16 @@ app.get('/data/create', function (req, res) {
         $set: {
           name: value.name,
           dbname: value.dbname,
-          description: value.description,
+          description: value.description
         }
       };
       dbo.collection('categories').updateOne(listingQuery, category, {upsert: true}, function (error, response) {
         if (error) throw error;
         console.log('Documents inserted or updated: ' + JSON.stringify(response));
-        res.jsonp({success: true});
       });
     });
   });
+  // List sample products
   const listingQuery = {name: 'mac air m1'};
   const listProducts = {
     $set: {
@@ -56,16 +57,42 @@ app.get('/data/create', function (req, res) {
       system_id: null
     }
   };
-// List sample products
   mongoClient.connect(url, function (error, database) {
     if (error) throw error;
     const dbo = database.db('niit-iproduct');
     dbo.collection('products').updateOne(listingQuery, listProducts, {upsert: true}, function (error, response) {
       if (error) throw error;
       console.log('Documents inserted or updated: ' + JSON.stringify(response));
-      res.jsonp(response);
     });
-  })
+  });
+  // List sample tags
+  const listTags = [
+    {name: 'Macbook Air 2020 M1', slug: 'macbook-air-2020-m1', description: 'Macbook Air 2020 M1', seo_id: null, system_id: null, is_google_trend: 'no'},
+    {name: 'Macbook Air M1', slug: 'macbook-air-m1', description: 'Macbook Air M1', seo_id: null, system_id: null, is_google_trend: 'no'},
+    {name: 'Macbook Air M1 2020', slug: 'macbook-air-m1-2020', description: 'Macbook Air M1 2020', seo_id: null, system_id: null, is_google_trend: 'no'}
+  ];
+  mongoClient.connect(url, function (error, database) {
+    if (error) throw error;
+    const dbo = database.db('niit-iproduct');
+    listTags.forEach(value => {
+      var listingQuery = {name: value.name};
+      var tag = {
+        $set: {
+          name: value.name,
+          slug: value.slug,
+          description: value.description,
+          seo_id: value.seo_id,
+          system_id: value.system_id,
+          is_google_trend: value.is_google_trend
+        }
+      };
+      dbo.collection('tags').updateOne(listingQuery, tag, {upsert: true}, function (error, response) {
+        if (error) throw error;
+        console.log('Documents inserted or updated: ' + JSON.stringify(response));
+      });
+    });
+  });
+  res.jsonp({success: true});
 });
 app.listen(port, env.SERVER_NAME, function () {
   console.log('Example app listening on port ' + port + '!')
