4a6b1b53bf1211658f73ceb93c34b1e193f68b25
tudao <nevarda9416@gmail.com>
2023-01-10 00:29:04 +0700

    - [ReactJS]: Draft tag management page

--- niit-iproduct/nodejs_expressjs/database/mongodb/tag.js
+++ niit-iproduct/nodejs_expressjs/database/mongodb/tag.js
@@ -8,13 +8,13 @@ const { ObjectId } = require('mongodb');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const env = process.env;
 const url = env.DATABASE_CONNECTION + '://' + env.DATABASE_HOST + ':' + env.DATABASE_PORT + '/';
-const port = env.DATABASE_PORT_PRODUCT_CRUD_DATA;
+const port = env.DATABASE_PORT_TAG_CRUD_DATA;
 const dbname = env.DATABASE_NAME;
-const collection_name = env.COLLECTION_PRODUCT_NAME;
+const collection_name = env.COLLECTION_TAG_NAME;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// Find product name like input data (GET)
+// Find tag name like input data (GET)
 app.get('/' + collection_name + '/find', function (req, res) {
   const search_name = req.query.name;
   mongoClient.connect(url, function (error, database) {
@@ -29,7 +29,7 @@ app.get('/' + collection_name + '/find', function (req, res) {
     });
   })
 });
-// List products (GET)
+// List tags (GET)
 app.get('/' + collection_name, function (req, res) {
   mongoClient.connect(url, function (error, database) {
     if (error) throw error;
@@ -45,7 +45,7 @@ app.get('/' + collection_name, function (req, res) {
     });
   })
 });
-// Add product (POST)
+// Add tag (POST)
 app.post('/' + collection_name + '/add', function (req, res) {
   console.log('Bearer token: ' + req.headers.authorization.split(' ')[1]);
   console.log(req.body);
@@ -71,7 +71,7 @@ app.post('/' + collection_name + '/add', function (req, res) {
     });
   })
 });
-// Edit product (GET)
+// Edit tag (GET)
 app.get('/' + collection_name + '/edit/:id', function (req, res) {
   const _id = req.params.id;
   mongoClient.connect(url, function (error, database) {
@@ -86,7 +86,7 @@ app.get('/' + collection_name + '/edit/:id', function (req, res) {
     });
   })
 });
-// Update product (POST)
+// Update tag (POST)
 app.post('/' + collection_name + '/edit/:id', function (req, res) {
   const listingQuery = {_id: new ObjectId(req.params.id)};
   console.log(req.body);
@@ -106,12 +106,12 @@ app.post('/' + collection_name + '/edit/:id', function (req, res) {
     const dbo = database.db(dbname);
     dbo.collection(collection_name).updateOne(listingQuery, updates, {upsert: true}, function (error, response) {
       if (error) throw error;
-      console.log('Product updated: ' + JSON.stringify(response));
+      console.log('Tag updated: ' + JSON.stringify(response));
       res.jsonp(response);
     });
   })
 });
-// Delete product (GET)
+// Delete tag (GET)
 app.get('/' + collection_name + '/delete/:id', function (req, res) {
   const listingQuery = {_id: new ObjectId(req.params.id)};
   mongoClient.connect(url, function (error, database) {
@@ -119,7 +119,7 @@ app.get('/' + collection_name + '/delete/:id', function (req, res) {
     const dbo = database.db(dbname);
     dbo.collection(collection_name).deleteOne(listingQuery, function (error, response) {
       if (error) throw error;
-      console.log('Product deleted');
+      console.log('Tag deleted');
       res.jsonp(response);
       database.close();
     });
