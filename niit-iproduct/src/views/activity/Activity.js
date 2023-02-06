baf664c53ff1e53e23cf815a13a065c99f2957e3
TuDT <tudt@ecomobi.com>
2023-02-06 13:20:17 +0700

    - [ReactJS]: Chuyển đổi activity pagination

--- niit-iproduct/src/views/activity/Activity.js
+++ niit-iproduct/src/views/activity/Activity.js
@@ -56,8 +56,8 @@ const Activity = () => {
   };
   const [data, setData] = useState([]);
   const [load, setLoad] = useState(0);
-  const [category, setCategory] = useState([]);
   const [activity, setActivity] = useState([]);
+  const [activities, setActivities] = useState([]);
   const [number, setNumber] = useState(1); // No of pages
   const [activityPerPage] = useState(LIMIT);
   const lastActivity = number * activityPerPage;
@@ -74,6 +74,7 @@ const Activity = () => {
     const getData = async () => {
       const data = await axios.get(url + ':' + activity_port + '/' + activity_collection);
       const dataJ = await data.data;
+      setActivities(dataJ);
       setData(dataJ);
     };
     getData();
@@ -181,12 +182,62 @@ const Activity = () => {
     setLoad(1);
   };
   const [t, i18n] = useTranslation('common');
+  const pagination =
+    <div className="my-3 text-center">
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          setNumber(1)
+        }}>
+        {t('paginate_first')}
+      </button>
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          if (number > 1)
+            setNumber(number - 1)
+          else
+            setNumber(1)
+        }}>
+        {t('paginate_previous')}
+      </button>
+      {pageNumber.map((element, index) => {
+        const className = (number === element) ? 'px-3 py-1 m-1 text-center btn btn-primary' : 'px-3 py-1 m-1 text-center btn btn-outline-dark'
+        return (
+          <span>{(element < number - 3 || element > number + 3 || element == number) &&
+          <button key={index}
+                  className={className}
+                  onClick={() => changePage(element)}>
+            {element}
+          </button>
+          }</span>
+        );
+      })}
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          if (number < Math.ceil(activities.length / activityPerPage))
+            setNumber(number + 1)
+          else
+            setNumber(Math.ceil(activities.length / activityPerPage))
+        }}>
+        {t('paginate_next')}
+      </button>
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          setNumber(Math.ceil(activities.length / activityPerPage))
+        }}>
+        {t('paginate_last')}
+      </button>
+    </div>
+  ;
   return (
     <CRow>
       <CCol xs={6}>
         <CCard className="mb-4">
           <CCardHeader>
-            <strong>{t('activity.add')}</strong>
+            <strong>{t('activity.add_or_edit')}</strong>
           </CCardHeader>
           <CCardBody>
             <CForm noValidate validated={validated} onSubmit={handleSubmit}>
@@ -256,6 +307,7 @@ const Activity = () => {
           <CFormInput onChange={e => changeInputSearch(e.target.value)} type="text" id="activitySearchName"
                       placeholder={t('activity.validate_input_subject')} value={activitySearch.name} required/>
         </div>
+        {pagination}
         <CTable bordered borderColor='primary'>
           <CTableHead>
             <CTableRow>
@@ -350,27 +402,7 @@ const Activity = () => {
             ))}
           </CTableBody>
         </CTable>
-        <div className="my-3 text-center">
-          <button
-            className="px-3 py-1 m-1 text-center btn-primary"
-            onClick={() => setNumber(number - 1)}>
-            {t('paginate_previous')}
-          </button>
-          {pageNumber.map((element, index) => {
-            return (
-              <button key={index}
-                      className="px-3 py-1 m-1 text-center btn-outline-dark"
-                      onClick={() => changePage(element)}>
-                {element}
-              </button>
-            );
-          })}
-          <button
-            className="px-3 py-1 m-1 text-center btn-primary"
-            onClick={() => setNumber(number + 1)}>
-            {t('paginate_next')}
-          </button>
-        </div>
+        {pagination}
       </CCol>
     </CRow>
   )
