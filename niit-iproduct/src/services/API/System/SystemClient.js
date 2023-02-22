3386bc670dc3a0d5e7cf4e30c38c8ecd9a2af34a
TuDT <tudt@ecomobi.com>
2023-02-22 18:30:43 +0700

    - [ReactJS + ExpressJS]: System delete data using token (JWT)

--- niit-iproduct/src/services/API/System/SystemClient.js
+++ niit-iproduct/src/services/API/System/SystemClient.js
@@ -13,8 +13,8 @@ const edit = (id, data, config) => {
   update(url, port, collection, id, data, config);
 };
 
-const deleteById = (id) => {
-  destroyById(url, port, collection, id);
+const deleteById = (id, data, config) => {
+  destroyById(url, port, collection, id, data, config);
 };
 
 export {create, edit, deleteById}
