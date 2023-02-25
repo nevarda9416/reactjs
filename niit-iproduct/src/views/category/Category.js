82e5ed9c85b06cde8948d0df20f8d691c5546195
tudao <nevarda9416@gmail.com>
2023-02-25 14:05:58 +0700

    - [ReactJS]: Category fix các lỗi còn tồn đọng liên quan đến reload data, mất data input onChange

--- niit-iproduct/src/views/category/Category.js
+++ niit-iproduct/src/views/category/Category.js
@@ -1,5 +1,5 @@
-import {React, useEffect, useState} from 'react'
-import {Link} from 'react-router-dom';
+import { React, useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
 import {
   CCard,
   CCardBody,
@@ -24,28 +24,29 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 import axios from 'axios';
-import {CKEditor} from '@ckeditor/ckeditor5-react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import {create, edit, deleteById} from "../../services/API/Category/CategoryClient";
-import {useTranslation} from "react-i18next";
+import { create, edit, deleteById } from "../../services/API/Category/CategoryClient";
+import { useTranslation } from "react-i18next";
 
 const url = process.env.REACT_APP_URL;
 const port = process.env.REACT_APP_PORT_DATABASE_MONGO_CATEGORY_CRUD_DATA;
 const Category = () => {
   const [validated, setValidated] = useState(false);
-  const [categorySearch, setCategorySearch] = useState({hits: []});
+  const [categorySearch, setCategorySearch] = useState({ hits: [] });
   const [id, setId] = useState(0);
-  const [action, setAction] = useState({hits: []});
-  const [loggedInUser, setLoggedInUser] = useState({hits: []});
+  const [action, setAction] = useState({ hits: [] });
+  const [loggedInUser, setLoggedInUser] = useState({ hits: [] });
   const LIMIT = process.env.REACT_APP_LIMIT_DATA_RETURN_TABLE;
   // Generate a random number and convert it to base 36 (0-9a-z): TOKEN CHƯA ĐƯỢC SỬ DỤNG
   const token = Math.random().toString(36).substr(2); // remove `0.`
   const config = {
-    headers: {Authorization: `Bearer ${token}`}
+    headers: { Authorization: `Bearer ${token}` }
   };
   const [state, setState] = useState({
     editor: null
   });
+  const inputRef = useRef(null);
   const loadData = async () => {
     const data = await axios.get(url + ':' + port + '/categories');
     const dataJ = await data.data;
@@ -71,7 +72,7 @@ const Category = () => {
     const loggedInUser = localStorage.getItem('userLoggedInfo');
     if (loggedInUser) {
       const foundUser = JSON.parse(loggedInUser);
-      setLoggedInUser(foundUser);      
+      setLoggedInUser(foundUser);
     }
     const getData = async () => {
       const data = await axios.get(url + ':' + port + '/categories');
@@ -86,8 +87,7 @@ const Category = () => {
         required
         onChange={(event, editor) => {
           const data = editor.getData();
-          console.log({event, editor, data});
-          changeTextarea(data);
+          console.log({ event, editor, data });
         }}
         onBlur={(_event, editor) => {
           console.log('Blur.', editor);
@@ -97,10 +97,10 @@ const Category = () => {
         }}
       />
     );
-    setState({...state, editor: editor});
+    setState({ ...state, editor: editor });
   }, []);
   const changeInput = (value) => {
-    setCategory({
+    setCategory({...category,
       name: value
     })
   };
@@ -113,7 +113,8 @@ const Category = () => {
     setData(dataJ);
   };
   const changeTextarea = (value) => {
-    setCategory({
+    setCategory({...category,
+      name: inputRef.current.value ?? '',
       description: value
     })
   };
@@ -138,7 +139,9 @@ const Category = () => {
       } else {
         create(category, config);
       }
-      loadData();
+      setTimeout(function(){
+        loadData();
+      }, 500);
     }
   };
   const editItem = (id) => {
@@ -146,7 +149,6 @@ const Category = () => {
     setAction('edit');
     axios.get(url + ':' + port + '/categories/edit/' + id)
       .then(res => {
-        setCategory(res.data);
         const editor = (
           <CKEditor
             editor={ClassicEditor}
@@ -158,7 +160,7 @@ const Category = () => {
             }}
             onChange={(event, editor) => {
               const data = editor.getData();
-              console.log({event, editor, data});
+              console.log({ event, editor, data });
               changeTextarea(data);
             }}
             onBlur={(_event, editor) => {
@@ -169,67 +171,72 @@ const Category = () => {
             }}
           />
         );
-        setState({...state, editor: editor});
+        setState({ ...state, editor: editor });
+      })
+      .catch(error => console.log(error));
+    axios.get(url + ':' + port + '/categories/edit/' + id)
+      .then(res => {
+        setCategory(res.data);
       })
       .catch(error => console.log(error));
   };
   const deleteItem = (id) => {
     setId(id);
-    setAction({'action': 'delete'});
+    setAction({ 'action': 'delete' });
     deleteById(id);
     loadData();
   };
   const [t] = useTranslation('common');
   const pagination =
-        <div className="my-3 text-center">
-            <button
-                className="px-3 py-1 m-1 text-center btn btn-primary"
-                onClick={() => {
-                    setNumber(1)
-                }}>
-                {t('paginate_first')}
+    <div className="my-3 text-center">
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          setNumber(1)
+        }}>
+        {t('paginate_first')}
+      </button>
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          if (number > 1)
+            setNumber(number - 1)
+          else
+            setNumber(1)
+        }}>
+        {t('paginate_previous')}
+      </button>
+      {pageNumber.map((element, index) => {
+        const className = (number === element) ? 'px-3 py-1 m-1 text-center btn btn-primary' : 'px-3 py-1 m-1 text-center btn btn-outline-dark'
+        return (
+          <span>{(element < number - 3 || element > number + 3 || element == number) &&
+            <button key={index}
+              className={className}
+              onClick={() => changePage(element)}>
+              {element}
             </button>
-            <button
-                className="px-3 py-1 m-1 text-center btn btn-primary"
-                onClick={() => {
-                    if (number > 1)
-                        setNumber(number - 1)
-                    else
-                        setNumber(1)
-                }}>
-                {t('paginate_previous')}
-            </button>
-            {pageNumber.map((element, index) => {
-                const className = (number === element) ? 'px-3 py-1 m-1 text-center btn btn-primary' : 'px-3 py-1 m-1 text-center btn btn-outline-dark'
-                return (
-                    <span>{(element < number - 3 || element > number + 3 || element == number) &&
-                        <button key={index}
-                            className={className}
-                            onClick={() => changePage(element)}>
-                            {element}
-                        </button>
-                    }</span>
-                );
-            })}
-            <button
-                className="px-3 py-1 m-1 text-center btn btn-primary"
-                onClick={() => {
-                    if (number < Math.ceil(categories.length / categoryPerPage))
-                        setNumber(number + 1)
-                    else
-                        setNumber(Math.ceil(categories.length / categoryPerPage))
-                }}>
-                {t('paginate_next')}
-            </button>
-            <button
-                className="px-3 py-1 m-1 text-center btn btn-primary"
-                onClick={() => {
-                    setNumber(Math.ceil(categories.length / categoryPerPage))
-                }}>
-                {t('paginate_last')}
-            </button>
-        </div>
-        ;
+          }</span>
+        );
+      })}
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          if (number < Math.ceil(categories.length / categoryPerPage))
+            setNumber(number + 1)
+          else
+            setNumber(Math.ceil(categories.length / categoryPerPage))
+        }}>
+        {t('paginate_next')}
+      </button>
+      <button
+        className="px-3 py-1 m-1 text-center btn btn-primary"
+        onClick={() => {
+          setNumber(Math.ceil(categories.length / categoryPerPage))
+        }}>
+        {t('paginate_last')}
+      </button>
+    </div>
+    ;
   return (
     <CRow>
       <CCol xs={4}>
@@ -242,8 +249,8 @@ const Category = () => {
               <div className="mb-3">
                 <CFormLabel htmlFor="categoryName">{t('category.label_name')}</CFormLabel>
                 <CFormInput onChange={e => changeInput(e.target.value)} type="text"
-                            feedbackInvalid={t('category.validate_input_name')} id="categoryName" value={category.name || ''}
-                            required/>
+                  feedbackInvalid={t('category.validate_input_name')} id="categoryName" value={category.name || ''}
+                  required ref={inputRef}/>
               </div>
               <div className="mb-3">
                 <CFormLabel htmlFor="exampleFormControlTextarea1">{t('category.label_description')}</CFormLabel>
@@ -251,8 +258,8 @@ const Category = () => {
                   {state.editor}
                 </div>
                 <CFormTextarea className="d-none" onChange={e => changeTextarea(e.target.value)}
-                               feedbackInvalid={t('category.validate_input_description')} id="categoryDescription" rows="3" required
-                               value={category.description || ''}/>
+                  feedbackInvalid={t('category.validate_input_description')} id="categoryDescription" rows="3" required
+                  value={category.description || ''} />
               </div>
               <div className="col-auto">
                 <CButton type="submit" className="mb-3">
@@ -267,7 +274,7 @@ const Category = () => {
         <div className="mb-3">
           <CFormLabel htmlFor="categoryName">{t('category.search')}</CFormLabel>
           <CFormInput onChange={e => changeInputSearch(e.target.value)} type="text"
-                      placeholder={t('category.validate_input_name')} value={categorySearch.name || ''} required/>
+            placeholder={t('category.validate_input_name')} value={categorySearch.name || ''} required />
         </div>
         {pagination}
         <CTable bordered borderColor='primary'>
@@ -286,16 +293,16 @@ const Category = () => {
                 <CTableDataCell>{item.name}</CTableDataCell>
                 <CTableDataCell>
                   {item.description &&
-                  <span>{item.description.replace(/<[^>]+>/g, '')}</span>
+                    <span>{item.description.replace(/<[^>]+>/g, '')}</span>
                   }
                 </CTableDataCell>
                 <CTableDataCell>
-                  <Link onClick={() => editItem(item._id)}><CIcon icon={cilPencil}/></Link>&nbsp;&nbsp;
+                  <Link onClick={() => editItem(item._id)}><CIcon icon={cilPencil} /></Link>&nbsp;&nbsp;
                   <Link onClick={() => {
                     if (window.confirm(t('category.confirm_delete'))) {
                       deleteItem(item._id);
                     }
-                  }}><CIcon icon={cilTrash}/></Link>
+                  }}><CIcon icon={cilTrash} /></Link>
                 </CTableDataCell>
               </CTableRow>
             ))}
